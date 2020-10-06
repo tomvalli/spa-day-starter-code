65204eb18eb80fb32aa6fc8207856729340c8d55
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-10-05 20:30:56 -0500

    Added server-side validation

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,23 +3,28 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        model.addAttribute(user);
+        if(errors.hasErrors()) {
+            model.addAttribute(errors);
+            return "user/add";
+        }
         if (user.getPassword().equals(verify)) {
            return "user/index";
         }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 5, max = 15, message = "Username should be between 5 an 15 characters")
     private String username;
+
+    @Email(message = "Please enter a properly formatted password")
     private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Please enter at least 6 characters for the password")
     private String password;
 
     public User() {
